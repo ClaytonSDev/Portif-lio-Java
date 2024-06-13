0a5c8eb23e9e4bcb022a1f1a40b56622a95056ff
projetos <claytonsalesdev@gmail.com>
2024-06-13 18:28:42 -0300

    .
    .

--- EstruturasDeDados/src/main/java/dataStructure/Stack.java
+++ EstruturasDeDados/src/main/java/dataStructure/Stack.java
@@ -6,13 +6,16 @@ public class Stack {
 
     private int height;
 
-    class Node {
+    public class Node {
         int value;
 
         Node next;
 
         Node (int value) {
             this.value = value;
+        }
+        public int getValue() {
+            return value;
         }
     }
 
